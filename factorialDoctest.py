039f306f415b8fe3e7c68907e65829437715c32c
Spartacusc <jekaicheev300@gmail.com>
2020-02-22 15:56:42 +0500

    testing changes

--- factorialDoctest.py
+++ factorialDoctest.py
@@ -12,11 +12,16 @@ def factorial(n):
     x = 1
     for i in range(1, n + 1):
         x *= i
-    return x
+    return
+
+
+def test_git():
+    pass
 
 
 if __name__ == "__main__":
     import doctest
+
     doctest.testmod()
 
 # print(factorial(5))
